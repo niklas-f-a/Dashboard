76493b717b2a7787f88ae5b07b5f5fc6f039c543
niklas-f-a <89448494+niklas-f-a@users.noreply.github.com>
2022-06-13 19:56:42 +0200

    Update registerSw.js

--- src/registerSw.js
+++ src/registerSw.js
@@ -5,6 +5,6 @@ export default function registerSW() {
   if('serviceWorker' in navigator && process.env.NODE_ENV === 'production'){
     navigator.serviceWorker.register('/service-worker.js')
     .then(() => console.log('service-worker registered'))
-    .catch(err => console.log('service-woker not registered'))
+    .catch(err => console.log('service-worker not registered'))
   }
 }
